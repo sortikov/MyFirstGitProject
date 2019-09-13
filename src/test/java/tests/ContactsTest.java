2697f2fec23b151f0f039095d78560b14d284983
Shahriyor Ortikov <sortikov@gmail.com>
2019-09-13 13:58:40 -0500

    Modified ContactsTest

--- src/test/java/tests/ContactsTest.java
+++ src/test/java/tests/ContactsTest.java
@@ -8,8 +8,8 @@ public class ContactsTest {
     @Test
     public void test1(){
 
-        System.out.println("driver open vytrack");
-        System.out.println("go to contacts page");
+        System.out.println("driver opens vytrack");
+        System.out.println("goes to the contacts page");
 
     }
 }
